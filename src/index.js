3e463846366c1994a25d5092077c85f0625a776e
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-28 18:51:50 +0100

    added props

--- src/index.js
+++ src/index.js
@@ -2,27 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //faker is a 3th party library that will generate random data
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+//Components are passed in as <CommentDetail/> instead of {CommentDetail}
 
 //FUNCTIONAL component
 const App = () => {
-    return (
+    return  (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={faker.image.avatar()} alt="avatar"/>
-                </a>
-                <div className="content">
-                    <a href="/" className="avatar">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today</span>
-                    </div>
-                    <div className="text">Nice blog post</div>
-                </div>
-            </div> 
+          <ApprovalCard>
+            <div>
+              <h4>Warning!</h4>
+              Are you sure you want to do this?
+            </div>
+          </ApprovalCard>
+    
+          <ApprovalCard>
+            <CommentDetail
+              author="Sam"
+              timeAgo="Today at 4:45PM"
+              content="Nice blog post"
+              avatar={faker.image.avatar()}
+            />
+          </ApprovalCard>
+    
+          <ApprovalCard>
+            <CommentDetail
+              author="Alex"
+              timeAgo="Today at 2:00AM"
+              content="I like the subject"
+              avatar={faker.image.avatar()}
+            />
+          </ApprovalCard>
+    
+          <ApprovalCard>
+            <CommentDetail
+              author="Jane"
+              timeAgo="Yesterday at 5:00PM"
+              content="I like the writing"
+              avatar={faker.image.avatar()}
+            />
+          </ApprovalCard>
         </div>
-    );
+      );
 }
 
 ReactDOM.render(
