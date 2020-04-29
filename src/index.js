eb3908ae86ca660c515aa12e565658cc74d8d7fe
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-29 12:30:45 +0100

    defaultProps, state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //We need 2 components (App(determine location+month weather)) -> pass it -> (SeasonDisplay(shows icons and text based on props))
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 //FUNCTIONAL component - NO ASYNC, WHERE we need to wait for a result/data.
@@ -22,21 +22,14 @@ import SeasonDisplay from './SeasonDisplay';
 //CLASS component - can manage STATE
 class App extends React.Component {
 
-    constructor(props){
-        super(props);//WE HAVE TO CALL IT
-        //WE HAVE TO INITIALIZE THE STATE
-        this.state = {lat: null};
-
-        //build-in the browser/ NORMAL JS
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                //setState({k:v}) -> ALWAYS: needs to be called to update the state.
-                //NEVER DO: this.state.lat = position.coords.latitude VERY BAD PRACTICE
-                this.setState({lat:position.coords.latitude})
-            },
-            (err) => console.log(err)
-        );
-    }
+    // constructor(props){
+    //     super(props);//WE HAVE TO CALL IT
+    //     //WE HAVE TO INITIALIZE THE STATE
+    //     //ONE WAY TO INIT STATE:
+    //     //this.state = {lat: null};
+    // }
+    //ALTERNATE WAY TO INIT STATE:
+    state = {lat: null}
 
     //execute function
     render(){
@@ -47,10 +40,41 @@ class App extends React.Component {
         //     (err) => console.log(err)
         // );
 
+        if(this.state.errorMessage && !this.state.lat){
+            return <SeasonDisplay errorMessage={this.state.errorMessage}/>
+            //return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            //here, you're passing a state as a prop to a child component
+            return <SeasonDisplay lat={this.state.lat}/>
+            //return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <Spinner message = "Please accept location permission"/>
+        // return <div>Loading...</div>
+
         //WE NEED TO RETURN html
-        return  (
-        <div>Latitude: {this.state.lat}</div>  
+    }
+
+    //IT'S recommended to do DATA LOADING HERE:
+    componentDidMount(){
+        //build-in the browser/ NORMAL JS
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //setState({k:v}) -> ALWAYS: needs to be called to update the state.
+                //NEVER DO: this.state.lat = position.coords.latitude VERY BAD PRACTICE
+                this.setState({lat:position.coords.latitude})
+            },
+            (err) => {
+                //setState({k:v}) -> will create a new property in the state if it doesn't exist.
+                this.setState({errorMessage: "Sorry latitude could not be retrieved."});
+            }
         );
+    }
+
+    componentDidUpdate(){
+        console.log("componentDidUpdate");
     }
 }
 
