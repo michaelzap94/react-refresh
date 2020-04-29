b7221cdf6af3bb6c52874dddcd8aa299de0ec4eb
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-29 17:53:05 +0100

    CSS grid system

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageCard = (props) => {
     //props = {key:'', image:''}
   
-    return <img src={props.image.urls.regular} alt=""/>;
+    return <img src={props.image.urls.regular} alt={props.image.description}/>;
   };
   
 export default ImageCard;
