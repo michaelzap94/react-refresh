31ee76ca63f9a2b94241290281bfedc3030718bf
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-29 18:28:37 +0100

    grid spans and react REFs

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
-const ImageCard = (props) => {
-    //props = {key:'', image:''}
+class ImageCard extends React.Component {
+    constructor(props) {
+      super(props);
   
-    return <img src={props.image.urls.regular} alt={props.image.description}/>;
-  };
+      this.state = { spans: 0 };
+    //create a ref so we can access the DOM properties
+      this.imageRef = React.createRef();
+    }
+  
+    componentDidMount() {
+        //add a vanilla JS eventlistener. html img triggers 'load' when the img has finished loading
+      this.imageRef.current.addEventListener('load', this._setSpans);
+    }
+  
+    _setSpans = () => {
+
+      const height = this.imageRef.current.clientHeight;//html img properties will be inside 'current'
+      //calculate how many spans(number of cells) this image will need to occupy.
+      const spans = Math.ceil(height / 10);
+      this.setState({ spans });
+    };
+  
+    render() {
+      const { description, urls } = this.props.image;
+  
+    //REMEMBER FROM CSS
+    /* this image needs to be allocated 2 cells worth of space, next image will be shown in next cell */
+    //grid-row-end: span 2; ===== {gridRowEnd: `span ${this.state.spans}`}
+      return (
+        <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+          <img ref={this.imageRef} alt={description} src={urls.regular} />
+        </div>
+      );
+    }
+  }
+  
   
 export default ImageCard;
