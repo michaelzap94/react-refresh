d1f81989e6d5c8d43c49208a71f259d2fa319c2a
Michael Zapata <mikeyfriends@hotmail.com>
2020-05-01 13:25:31 +0100

    initial setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-// import PostList from './PostList';
+import {BrowserRouter, Route} from 'react-router-dom';
 
+const PageOne = () => {
+    return <div>PageOne</div>
+}
+
+const PageTwo = () => {
+    return <div>PageTwo</div>
+}
+
+//you can have duplicated Route components both pointing at the same path: as you can have deeply nested components
+//exact -> so that you only show the page if the '/path' is exact:
+//eg: NO exact -> Page1) '/' Page2) '/any' -> will show both Page1 and Page2
+// exact === exact={true}
+// it prevents the urlPath.contains('/yourpathinRoute') and changes it to urlPath === '/yourpathinRoute'
 
 function App() {
   return (
     <div className="ui container">
-        App
+        <BrowserRouter>
+            <Route path="/" exact component={PageOne}/>
+            <Route path="/" exact component={PageOne}/>
+            <Route path="/pageTwo" component={PageTwo}/>
+        </BrowserRouter>
     </div>
   );
 }
