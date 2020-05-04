a3d8955fe315cc10b8b34adc3a43a42b9a8e29cb
Michael Zapata <mikeyfriends@hotmail.com>
2020-05-04 11:44:05 +0100

    form finished

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 
 class StreamCreate extends React.Component {
-  _renderError({ error, touched }) {
+
+  _renderError = (meta) => {
+    const { error, touched } = meta;
     if (touched && error) {
       return (
         <div className="ui error message">
@@ -14,15 +16,17 @@ class StreamCreate extends React.Component {
 
   _renderInput = (formProps) => {
     //This props are only for the Field Component.
-    //console.log(formProps);
     //the 'name' prop will be inside input, other labels passed in the Field component will be outside eg: label
-    //formProps -->  {label:'', meta:{}, input: { name: ''}}
+    //formProps -->  {label:'', meta:{touched: false, valid: false, visited: true, error: "You must enter a title"}, input: { name: ''}}
+    
+    //OPTIONAL class to make the field red and inlcude erro message
+    const classNameWithError = `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`;
     return (
-      <div className="field">
+      <div className={classNameWithError}>
         <label>{formProps.label}</label>
         {/*OLD AND LONG <input onChange={formProps.input.onChange} value={formProps.input.value} autoComplete="off" /> */}
-        <input {...formProps.input} />
-        <div>{formProps.meta.error}</div>
+        <input {...formProps.input} autoComplete="off"/>
+        {this._renderError(formProps.meta)}
       </div>
     );
   };
@@ -30,17 +34,18 @@ class StreamCreate extends React.Component {
   _onSubmit(formValues){
     // event does not exist in redux-form as they'll handle it for us, it will pass form values
     // event.proventDefault();// no need as redux-form will handle this
-    console.log(formValues);//{title: "fsdfdf", description: "fdsfsd"}
+    //console.log(formValues);//{title: "fsdfdf", description: "fdsfsd"}
 
   }
 
   render(){
-    console.log(this.props);
+    //console.log(this.props);
     //NO redux-form: onSubmit={this._onSubmit}
     //with redux-form: onSubmit={this.props.handleSubmit(this._onSubmit)}//this.props.handleSubmit is provided by redux-form
+    //SemanticUI will hide className='error' by default so we need to include 'error' in the form
     return (
       <div className="ui container">
-          <form className="ui form" onSubmit={this.props.handleSubmit(this._onSubmit)}>
+          <form className="ui form error" onSubmit={this.props.handleSubmit(this._onSubmit)}>
             <Field name='title' label="Enter Title" component={this._renderInput}/>
             <Field name='description' label="Enter Description" component={this._renderInput}/>
             <button className="ui button primary">Submit</button>
@@ -55,9 +60,10 @@ class StreamCreate extends React.Component {
 //if there's any error in this form, it will pass the error object to the component={FUNCTION} of the Field with the same name as the error key:
 //eg: formValues.title -> will pass the error to <Field name='title' component={this._renderInput}/>, 
 //so the error can be fetched from the this._renderInput(formProps) function-> using formProps.meta.error
+//this function will RUN ALL THE TIME, and will prevent the form submission(keyenter or submit click) till no errors
 const _validate = formValues => {
   // it will pass form values so we can validate it
-  console.log(formValues);//{title: "fsdfdf", description: "fdsfsd"}
+  //console.log(formValues);//{title: "fsdfdf", description: "fdsfsd"}
 
   const errors = {};//if empty then no errors
 
