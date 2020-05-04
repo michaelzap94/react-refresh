08c71a49a4deaffe694874314d8bc614435039e2
Michael Zapata <mikeyfriends@hotmail.com>
2020-05-04 10:35:15 +0100

    reducer form added

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+import {reducer as formReducer} from 'redux-form';
 import authReducer from './authReducer';
 
+//the KEY of the formReducer must be form.
 export default combineReducers({
-    authReducer
+    authReducer,
+    form: formReducer
 });
