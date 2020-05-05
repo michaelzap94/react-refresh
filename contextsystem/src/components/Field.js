2b1bd0f11a352de482fc1ea4f99c15fc6cbd500a
Michael Zapata <mikeyfriends@hotmail.com>
2020-05-05 12:49:45 +0100

    basic context

--- contextsystem/src/components/Field.js
+++ contextsystem/src/components/Field.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
+    //this has to be called exactly 'contextType' just like 'state' has to be called 'state'
+    static contextType = LanguageContext;
 
-  render() {
-    const text = true ? 'Name' : 'Naam';
+    render() {
+        console.log(this.context);
+        const text = this.context === 'english' ? 'Name' : 'Naam';
 
-    return (
-      <div className="ui field">
-        <label>{text}</label>
-        <input />
-      </div>
-    );
-  }
+        return (
+            <div className="ui field">
+            <label>{text}</label>
+            <input />
+            </div>
+        );
+    }
 }
+
+//OR HERE: Field.contextType = LanguageContext;
+
 
 export default Field;
---
+++ contextsystem/src/contexts/LanguageContext.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+//React.createContext()//will create the contextObject
+const Context = React.createContext('english');
+
+export default Context;
