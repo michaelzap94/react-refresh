0f0aaee89e0c17a124f5ad6e5da84ac3f149610e
Michael Zapata <mikeyfriends@hotmail.com>
2020-05-05 12:21:43 +0100

    init app

---
+++ contextsystem/src/components/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import UserCreate from './UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+          App
+      </div>
+    );
+  }
+}
+
+export default App;
