4a6dae287d3fe9c328ae493ca19fbdd044bdf5af
Michael Zapata <mikeyfriends@hotmail.com>
2020-05-05 14:53:25 +0100

    replace state with useState elements

--- hooks/src/components/App.js
+++ hooks/src/components/App.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class App extends React.Component {
-  state = {resource: 'posts'};
-  render() {
+const App = () => {
+    //state = {resource: 'posts'};
+    const [resource, setResource] = useState('posts');
     return (
         <div>
-          <div>
             <div>
-                <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-                <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
+            <div>
+                {/* <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
+                <button onClick={() => this.setState({resource: 'todos'})}>Todos</button> */}
+                <button onClick={() => setResource('posts')}>Posts</button>
+                <button onClick={() => setResource('todos')}>Todos</button>
             </div>
-            {this.state.resource}
-          </div>
+            {/* {this.state.resource} */}
+            {resource}
+            </div>
         </div>
-      );
-  }
-};
+    );
+}
+
+// Basic class based
+// class App extends React.Component {
+//   state = {resource: 'posts'};
+//   render() {
+//     return (
+//         <div>
+//           <div>
+//             <div>
+//                 <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
+//                 <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
+//             </div>
+//             {this.state.resource}
+//           </div>
+//         </div>
+//       );
+//   }
+// };
+
+
 
 export default App;
